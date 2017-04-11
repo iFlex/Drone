77af3a5292afdd8e4c4cdc4987fa5c9d37c7756a
iFlex <milorad.liviu.felix@gmail.com>
2017-04-11 13:27:41 +0100

    fixes and mixes

--- server.py
+++ server.py
@@ -3,54 +3,45 @@ import pigpio
 import json
 
 pi = pigpio.pi()
-cmdIndex = 0;
-commands = [0,0,0,0]
 config = []
-DEBUG = False
+DEBUG = True
 
 print("Reading Config")
 with open('config.json', 'r') as f:
     config = json.load(f)
 
 print("Starting Server")
-conn = socket(AF_INET,SOCK_STREAM)
+conn = socket(AF_INET,SOCK_DGRAM)
 conn.bind(('localhost',config["port"]))
-conn.listen(1);
 
 def applyCommands(cmds):
     global config
     if DEBUG == True:
-        print("Applying commands:"+str(cmds))
+        dbg = ""
+        for c in cmds:
+            dbg += ord(c)+" ";
+        print dbg  
+    
+    pi.set_PWM_dutycycle(config["pins"]["THROTTLE"], cmds[0])
+    pi.set_PWM_dutycycle(config["pins"]["YAW"],      cmds[1])
+    pi.set_PWM_dutycycle(config["pins"]["PITCH"],    cmds[2])
+    pi.set_PWM_dutycycle(config["pins"]["ROLL"],     cmds[3])
 
-    for c in cmds:
-        if len(c) == 0:
-            print "INVALID COMMAND:'"+c+"'"
-            return
-        
-    pi.set_PWM_dutycycle(config["pins"]["THROTTLE"], ord(cmds[0]))
-    pi.set_PWM_dutycycle(config["pins"]["YAW"], ord(cmds[1]))
-    pi.set_PWM_dutycycle(config["pins"]["PITCH"], ord(cmds[2]))
-    pi.set_PWM_dutycycle(config["pins"]["ROLL"], ord(cmds[3]))
-
-while True:
-    client = conn.accept()
-    print("New connection:"+str(client[0]))
-    controller = client[0];
+alive = True
+def timeoutThread():
+    #todo check timestamp
     while True:
-        try:
-            byte = controller.recv(1)
-            if len(byte) == 0:
-                print ("Connection closed");
-                break
-            
-	    commands[cmdIndex] = byte
-            
-            if cmdIndex == len(commands)-1:
-                applyCommands(commands)
-            cmdIndex += 1            
-            cmdIndex %= 4
+        alive = False
+        #sleep
+        #if alive == False:
+        #    applyCommands();
 
-        except Exception as e:
-	    print ("Broke connection to client:"+str(e))            
-            controller.close()
-            break
+#threading.start_thread(timeoutThread,());
+while True:
+    try:
+        commands, address = conn.recv(4)
+        applyCommands(commands)
+        #timestamp last update
+
+    except Exception as e:
+    print ("Broke connection to client:"+str(e))
